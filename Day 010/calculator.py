934ed229c72217bf42d4a3ffed61c39950acebbc
Keya Barua <52108484+KeyaBarua@users.noreply.github.com>
2024-07-26 16:33:56 +0600

    Update calculator.py

--- Day 010/calculator.py
+++ Day 010/calculator.py
@@ -34,34 +34,28 @@ def calculator():
 
     first_number = float(input("What's the first number?: "))
 
-    # Flag
-    turn_off_calculator = False
+    # Displaying the operation symbols
+    for key in operations:
+        print(key)
 
-    while not turn_off_calculator:
-        # Displaying the operation symbols
-        for key in operations:
-            print(key)
+    oper_symbol = input("Pick an operation: ")
+    # Getting the value from the operations dictionary
+    operation_to_do = operations[oper_symbol]
 
-        oper_symbol = input("Pick an operation: ")
-        # Getting the value from the operations dictionary
-        operation_to_do = operations[oper_symbol]
+    next_number = float(input("What's the next number?: "))
+    result = operation_to_do(first_number, next_number)
 
-        next_number = float(input("What's the next number?: "))
-        result = operation_to_do(first_number, next_number)
+    print(f"{first_number} {oper_symbol} {next_number} = {result}")
 
-        print(f"{first_number} {oper_symbol} {next_number} = {result}")
-
-        to_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation, "
+    to_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation, "
                             f"or type 'AC' to exit: "
                             f"").lower()
-        if to_continue == "y":
-            # Making the first number equal to result to continue calculating with the previous result
-            first_number = result
-        elif to_continue == "n":
-            clear()
-            calculator()
-        else:
-            turn_off_calculator = True
+    if to_continue == "y":
+        # Making the first number equal to result to continue calculating with the previous result
+        first_number = result
+    elif to_continue == "n":
+        clear()
+        calculator()
 
 
 calculator()
