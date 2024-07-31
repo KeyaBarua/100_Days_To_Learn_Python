1414a0ef9aa2d0eebc2eff442a91b0983b1a5854
Keya Barua <52108484+KeyaBarua@users.noreply.github.com>
2024-07-31 18:09:17 +0600

    Create game_data.py

---
+++ Day 013 - 014/game_data.py
@@ -0,0 +1,603 @@
+accounts = [
+    {
+        "name": "Instagram",
+        "followers": "674.16 M",
+        "description": "social media platform",
+        "country": "USA"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "followers": "633.87 M",
+        "description": "footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Lionel Messi",
+        "followers": "504.03 M",
+        "description": "footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Selena Gomez",
+        "followers": "426.32 M",
+        "description": "musician and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Kylie Jenner",
+        "followers": "398.92 M",
+        "description": "model and TV personality",
+        "country": "USA"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "followers": "396.82",
+        "description": "actor and professional wrestler",
+        "country": "USA"
+    },
+    {
+        "name": "Ariana Grande",
+        "followers": "378.42 M",
+        "description": "musician and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Kim Kardashian",
+        "followers": "361.87 M",
+        "description": "model and TV personality",
+        "country": "USA"
+    },
+    {
+        "name": "Beyonce",
+        "followers": "317.9 M",
+        "description": "musician",
+        "country": "USA"
+    },
+    {
+        "name": "Khloe Kardashian",
+        "followers": "308.25 M",
+        "description": "TV personality and model",
+        "country": "USA"
+    },
+    {
+        "name": "Nike",
+        "followers": "305.67 M",
+        "description": "athletic footwear and apparel company",
+        "country": "USA"
+    },
+    {
+        "name": "Justin Bieber",
+        "followers": "293.43 M",
+        "description": "musician",
+        "country": "Canada"
+    },
+    {
+        "name": "Kendall Jenner",
+        "followers": "292.79 M",
+        "description": "model and TV personality",
+        "country": "USA"
+    },
+    {
+        "name": "Taylor Swift",
+        "followers": "283.82 M",
+        "description": "musician",
+        "country": "USA"
+    },
+    {
+        "name": "National Geographic",
+        "followers": "282.7 M",
+        "description": "magazine",
+        "country": "USA"
+    },
+    {
+        "name": "Virat Kohli",
+        "followers": "270.51 M",
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "followers": "252.19",
+        "description": "musician and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Nicki Minaj",
+        "followers": "229.03 M",
+        "description": "musician and actress",
+        "country": "Trinidad and Tobago"
+    },
+    {
+        "name": "Kourtney Kardashian",
+        "followers": "222.87 M",
+        "description": "TV personality and model",
+        "country": "USA"
+    },
+    {
+        "name": "Neymar",
+        "followers": "222.68 M",
+        "description": "footballer",
+        "country": "Brazil"
+    },
+    {
+        "name": "Miley Cyrus",
+        "followers": "215.96 M",
+        "description": "musician and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Katy Perry",
+        "followers": "206.54 M",
+        "description": "musician",
+        "country": "USA"
+    },
+    {
+        "name": "Zendaya",
+        "followers": "183.58 M",
+        "description": "actress and musician",
+        "country": "USA"
+    },
+    {
+        "name": "Kevin Hart",
+        "followers": "179.7 M",
+        "description": "comedian and actor",
+        "country": "USA"
+    },
+    {
+        "name": "Cardi B",
+        "followers": "166.89 M",
+        "description": "musician",
+        "country": "USA"
+    },
+    {
+        "name": "Real Madrid CF",
+        "followers": "163.25 M",
+        "description": "football club",
+        "country": "Spain"
+    },
+    {
+        "name": "LeBron James",
+        "followers": "159.76 M",
+        "description": "basketball player",
+        "country": "USA"
+    },
+    {
+        "name": "Demi Lovato",
+        "followers": "156.02 M",
+        "description": "musician and actress",
+        "country": "USA"
+    },
+    {
+        "name": "Rihanna",
+        "followers": "151.83 M",
+        "description": "musician",
+        "country": "Barbados"
+    },
+    {
+        "name": "Drake",
+        "followers": "146 M",
+        "description": "musician",
+        "country": "Canada"
+    },
+    {
+        "name": "Chris Brown",
+        "followers": "145.11 M",
+        "description": "musician",
+        "country": "USA"
+    },
+    {
+        "name": "Ellen DeGeneres",
+        "followers": "138.8 M",
+        "description": "comedian and TV personality",
+        "country": "USA"
+    },
+    {
+        "name": "FC Barcelona",
+        "followers": "127.76 M",
+        "description": "football club",
+        "country": "Spain"
+    },
+    {
+        "name": "Kylian Mbappe",
+        "followers": "119.43 M",
+        "description": "footballer",
+        "country": "France"
+    },
+    {
+        "name": "Billie Eilish",
+        "followers": "119.37 M",
+        "description": "musician",
+        "country": "USA"
+    },
+    {
+        "name": "UEFA Champions League",
+        "followers": "115.57 M",
+        "description": "club football competition",
+        "country": "Europe"
+    },
+    {
+        "name": "Gal Gadot",
+        "followers": "108.76 M",
+        "description": "actress and model",
+        "country": "Israel"
+    },
+    {
+        "name": "Lisa",
+        "followers": "103.74 M",
+        "description": "musician",
+        "country": "Thailand"
+    },
+    {
+        "name": "Vin Diesel",
+        "followers": "102.81 M",
+        "description": "actor",
+        "country": "USA"
+    },
+    {
+        "name": "NASA",
+        "followers": "97.76 M",
+        "description": "space agency",
+        "country": "USA"
+    },
+    {
+        "name": "Priyanka Chopra",
+        "followers": "91.51 M",
+        "description": "actress and musician",
+        "country": "India"
+    },
+    {
+        "name": "Narendra Modi",
+        "followers": "91.2 M",
+        "description": "politician",
+        "country": "India"
+    },
+    {
+        "name": "Shakira",
+        "followers": "90.42 M",
+        "description": "musician",
+        "country": "Colombia"
+    },
+    {
+        "name": "Shraddha Kapoor",
+        "followers": "90.36 M",
+        "description": "actress",
+        "country": "India"
+    },
+    {
+        "name": "NBA",
+        "followers": "88.4 M",
+        "description": "professional basketball league",
+        "country": "USA"
+    },
+    {
+        "name": "Dua Lipa",
+        "followers": "88.09 M",
+        "description": "musician",
+        "country": "UK"
+    },
+    {
+        "name": "David Beckham",
+        "followers": "88.08 M",
+        "description": "footballer",
+        "country": "UK"
+    },
+    {
+        "name": "Snoop Dogg",
+        "followers": "87.65 M",
+        "description": "musician",
+        "country": "USA"
+    },
+    {
+        "name": "Jennie",
+        "followers": "85.11 M",
+        "description": "singer and rapper",
+        "country": "South Korea"
+    },
+    {
+        "name": "Alia Bhatt",
+        "followers": "84.96 M",
+        "description": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Khaby Lame",
+        "followers": "81.11 M",
+        "description": "gamer and tiktoker",
+        "country": "Senegal"
+    },
+    {
+        "name": "Katrina Kaif",
+        "followers": "80.09 M",
+        "description": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Deepika Padukone",
+        "followers": "79.51 M",
+        "description": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Jisoo",
+        "followers": "79.04 M",
+        "description": "singer",
+        "country": "South Korea"
+    },
+    {
+        "name": "Neha Kakkar",
+        "followers": "78.76 M",
+        "description": "singer",
+        "country": "India"
+    },
+    {
+        "name": "Rose",
+        "followers": "78.62 M",
+        "description": "singer",
+        "country": "South Korea"
+    },
+    {
+        "name": "Gigi Hadid",
+        "followers": "77.81 M",
+        "description": "model",
+        "country": "USA"
+    },
+    {
+        "name": "Karim Benzema",
+        "followers": "76.85 M",
+        "description": "footballer",
+        "country": "France"
+    },
+    {
+        "name": "Ronaldo de Assis Moreira",
+        "followers": "76.52 M",
+        "description": "footballer",
+        "country": "Brazil"
+    },
+    {
+        "name": "Raffi Ahmed",
+        "followers": "76.09 M",
+        "description": "media personality",
+        "country": "Indonesia"
+    },
+    {
+        "name": "Victoria's Secret",
+        "followers": "75.99 M",
+        "description": "lingerie brand",
+        "country": "USA"
+    },
+    {
+        "name": "BTS Official",
+        "followers": "75.09 M",
+        "description": "boy band",
+        "country": "South Korea"
+    },
+    {
+        "name": "Premier League",
+        "followers": "74.85 M",
+        "description": "football league",
+        "country": "UK"
+    },
+    {
+        "name": "Abel Makkonen Tesfaye",
+        "followers": "74.75 M",
+        "description": "singer, songwriter and record producer",
+        "country": "Canada"
+    },
+    {
+        "name": "Emma Watson",
+        "followers": "74.57 M",
+        "description": "actress",
+        "country": "UK"
+    },
+    {
+        "name": "433",
+        "followers": "74.31 M",
+        "description": "community",
+        "country": "Netherlands"
+    },
+    {
+        "name": "Urvashi Rautela",
+        "followers": "73.31 M",
+        "description": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Justin Timberlake",
+        "followers": "72.62 M",
+        "description": "musician and actor",
+        "country": "USA"
+    },
+    {
+        "name": "Shawn Mendes",
+        "followers": "72.21 M",
+        "description": "musician",
+        "country": "Canada"
+    },
+    {
+        "name": "Jacqueline Fernandez",
+        "followers": "70.2 M",
+        "description": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Karol G",
+        "followers": "69.6 M",
+        "description": "singer and songwriter",
+        "country": "Colombia"
+    },
+    {
+        "name": "Will Smith",
+        "followers": "69.51 M",
+        "description": "singer",
+        "country": "USA"
+    },
+    {
+        "name": "Salman Khan",
+        "followers": "68.84 M",
+        "description": "actor",
+        "country": "India"
+    },
+    {
+        "name": "Anushka Sharma",
+        "followers": "68.28 M",
+        "description": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Marcelo Vieira",
+        "followers": "68.09 M",
+        "description": "footballer",
+        "country": "Brazil"
+    },
+    {
+        "name": "Akshay Kumar",
+        "followers": "67.68 M",
+        "description": "actor",
+        "country": "India"
+    },
+    {
+        "name": "Marvel Entertainment",
+        "followers": "65.86 M",
+        "description": "entertainment company",
+        "country": "USA"
+    },
+    {
+        "name": "Kim Taehyung",
+        "followers": "65.77 M",
+        "description": "singer and songwriter",
+        "country": "USA"
+    },
+    {
+        "name": "Tom Holland",
+        "followers": "65.47 M",
+        "description": "actor",
+        "country": "UK"
+    },
+    {
+        "name": "Camila Cabello",
+        "followers": "65.21 M",
+        "description": "singer and songwriter",
+        "country": "Cuba"
+    },
+    {
+        "name": "Anitta",
+        "followers": "64.57 M",
+        "description": "singer",
+        "country": "Brazil"
+    },
+    {
+        "name": "Paris Saint-Germain",
+        "followers": "64.38 M",
+        "description": "football club",
+        "country": "France"
+    },
+    {
+        "name": "Zlatan Ibrahimovic",
+        "followers": "64.25 M",
+        "description": "footballer",
+        "country": "Sweden"
+    },
+    {
+        "name": "Maluma",
+        "followers": "64.17 M",
+        "description": "musician",
+        "country": "Colombia"
+    },
+    {
+        "name": "Manchester United",
+        "followers": "63.92 M",
+        "description": "football club",
+        "country": "UK"
+    },
+    {
+        "name": "Sergio Ramos",
+        "followers": "63.75 M",
+        "description": "footballer",
+        "country": "Spain"
+    },
+    {
+        "name": "Millie Bobby Brown",
+        "followers": "63.43 M",
+        "description": "actress",
+        "country": "UK"
+    },
+    {
+        "name": "Mohamed Salah",
+        "followers": "63.28 M",
+        "description": "footballer",
+        "country": "Egypt"
+    },
+    {
+        "name": "Zara",
+        "followers": "61.86 M",
+        "description": "fashion company",
+        "country": "Spain"
+    },
+    {
+        "name": "Leonardo DiCaprio",
+        "followers": "61.77 M",
+        "description": "actor",
+        "country": "USA"
+    },
+    {
+        "name": "Paul Labile Pogba",
+        "followers": "61.74 M",
+        "description": "footballer",
+        "country": "France"
+    },
+    {
+        "name": "Zac Efron",
+        "followers": "61.72 M",
+        "description": "actor",
+        "country": "USA"
+    },
+    {
+        "name": "Disha Patani",
+        "followers": "61.45 M",
+        "description": "actress",
+        "country": "India"
+    },
+    {
+        "name": "Bella Hadid",
+        "followers": "61.29 M",
+        "description": "model",
+        "country": "USA"
+    },
+    {
+        "name": "Juventus",
+        "followers": "60.48 M",
+        "description": "football club",
+        "country": "Italy"
+    },
+    {
+        "name": "Georgina Rodríguez",
+        "followers": "60.25 M",
+        "description": "social media influencer and model",
+        "country": "Spain"
+    },
+    {
+        "name": "Whindersson Nunes",
+        "followers": "59.42 M",
+        "description": "comedian",
+        "country": "Brazil"
+    },
+    {
+        "name": "Chris Hemsworth",
+        "followers": "59.35 M",
+        "description": "actor",
+        "country": "Australia"
+    },
+    {
+        "name": "Joko Widodo",
+        "followers": "58.73 M",
+        "description": "politician and businessman",
+        "country": "Indonesia"
+    },
+    {
+        "name": "Mr. Beast",
+        "followers": "58.27 M",
+        "description": "You Tuber and social media influencer",
+        "country": "USA"
+    },
+]
+
